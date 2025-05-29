ed1f7679e3a25cc8207a49e0e2f1629db9e68012
laisdeveloper <laiscc575@gmail.com>
2025-05-29 15:27:21 -0300

    feat: adicionando DIO/UMLIphone e DIO/BancoDigital

---
+++ github/DIO/BancoDigitalComPOO/src/apps/Main.java
@@ -0,0 +1,20 @@
+package apps;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cliente venilton = new Cliente();
+		venilton.setNome("Venilton");
+		
+		Conta cc = new ContaCorrente(venilton);
+		Conta poupanca = new ContaPoupanca(venilton);
+
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+
+}
---
+++ github/DIO/BancoDigitalComPOO/src/entities/Cliente.java
@@ -0,0 +1,13 @@
+package entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+public class Cliente {
+
+	private String nome;
+    private String cpf;
+
+}
